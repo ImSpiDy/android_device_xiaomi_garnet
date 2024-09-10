c7d12d32a6aea4c6134ef7f72984d1d84fd481cb
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-09-11 03:14:50 +0530

    garnet: Inherit priv-keys if-exist
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- device.mk
+++ device.mk
@@ -461,6 +461,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# Sign key
+-include vendor/lineage-priv/keys/keys.mk
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
