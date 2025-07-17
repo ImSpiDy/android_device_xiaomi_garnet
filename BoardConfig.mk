ea42b355a7ef6cf958af619e514d28c28881b1c8
ImSpiDy <SpiDy2713@gmail.com>
2025-07-17 23:17:23 +0000

    garnet: Inherit device_xiaomi_common if-exist
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,5 +234,8 @@ WIFI_FEATURE_HOSTAPD_11AX := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# common
+-include device/xiaomi/common/BoardConfig.mk
+
 # Vendor
 include vendor/xiaomi/garnet/BoardConfigVendor.mk
